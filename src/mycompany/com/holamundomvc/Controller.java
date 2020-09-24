6d7ffb822b1216b893db96dc9b51a036ecd4492e
Aketza19 <aketzae5@gmail.com>
2020-09-24 09:04:51 +0200

    First commit

---
+++ src/mycompany/com/holamundomvc/Controller.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycompany.com.holamundomvc;
+
+import mycompany.com.holamundomvc.model.Model;
+import mycompany.com.holamundomvc.view.View;
+
+/**
+ *
+ * @author Aketza
+ */
+public class Controller {
+    
+    public void run(View view, Model model){
+        
+    }
+    
+}
---
+++ src/mycompany/com/holamundomvc/view/ViewImplementation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycompany.com.holamundomvc.view;
+
+/**
+ * Implements the view that shows the message in a text mode.
+ * @author Aketza
+ */
+public class ViewImplementation implements View {
+
+     /**
+     * Shows a greeting in UI.
+     * @param greeting A String containing the greeting to be shown.
+     */
+    
+    @Override
+    public void showGreeting (String greeting){
+        System.out.println(greeting);
+    }
+}
